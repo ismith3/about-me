1938139baea36ea156e1343bb41f902e4157e7c2
Ian Smith <ianmsmith17@gmail.com>
2019-04-09 12:02:55 -0500

    added questions page

---
+++ js/questions.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var name = prompt('Hello! What is your name?');
+var origin = prompt('Hi, ' + name + '. Where are you from?');
+var color = prompt('What is your favorite color?');
+var movie = prompt('What is your favorite movie?');
+
+confirm("So, your name is " + name + ", you're from " + origin + ", your favorite color is " + color + ", and your favorite movie is " + movie + ". Is that correct?");
